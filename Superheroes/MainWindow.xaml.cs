b768153133f384cdd7e8961fc353d9788fc6ee20
PabloS <pablo.sempere@iesdoctorbalmis.com>
2022-11-04 13:34:28 +0100

    Agregar archivos de proyecto.

---
+++ Superheroes/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Superheroes.VistasModelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Superheroes
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainWindowVN vm = new MainWindowVN();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            this.DataContext = vm;
+            /*
+             cantidadSupers = listaSupers.Count
+             numeroSupers_TextBlock = heroe + "/" + cantidadSupers
+             fondo_DockPanel.DataContext = listaSupers[heroe - 1]
+             */
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Image boton = (Image)sender;
+            if (boton.Tag.ToString() == "-")
+            {
+                vm.Retroceder();
+            }
+            else
+            {
+                vm.Avanzar();
+            }
+        }
+    }
+}
---
+++ Superheroes/VistasModelo/MainWindowVN.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheroes.VistasModelo
+{
+    class MainWindowVN : INotifyPropertyChanged
+    {
+        private Superheroe personaje;
+
+        public Superheroe Personaje
+        {
+            get { return personaje; }
+            set 
+            { 
+                personaje = value;
+                this.NotifyPropertyChanged("Personaje");
+            }
+        }
+
+        private int posicion;
+
+        public int Posicion
+        {
+            get { return posicion; }
+            set 
+            { 
+                posicion = value;
+                this.NotifyPropertyChanged("Posicion");
+            }
+        }
+
+        private int total;
+
+        public int Total
+        {
+            get { return total; }
+            set 
+            { 
+                total = value;
+                this.NotifyPropertyChanged("Total");
+            }
+        }
+
+        private List<Superheroe> listaSuperheroes = Superheroe.GetSamples();
+
+        public MainWindowVN() 
+        {
+            this.personaje = listaSuperheroes[0];
+            this.posicion = 1;
+            this.total = listaSuperheroes.Count;
+        }
+
+        public void Avanzar()
+        {
+            if(Posicion < Total) 
+            {
+                //IMPORTANTE usar la propiedad (en mayuscula la primera), para que el setter con el notify salte
+                Posicion++;
+                Personaje = listaSuperheroes[Posicion - 1];
+            }
+            
+        }
+        public void Retroceder()
+        {
+            if(Posicion > 1)
+            {
+                Posicion--;
+                Personaje = listaSuperheroes[Posicion - 1];
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
